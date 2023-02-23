8f12b519032df62a39c4e1600a472767719952e0
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-23 17:09:16 +0300

    Added Task19 solution

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string PalyndromeNum(){
+    string? userInputErrorTest = Console.ReadLine();
+    string num = userInputErrorTest ?? "NullErrorException";
+    bool flag = true;
+
+    if (num == "NullErrorException") {
+        return "Error";
+    }
+    
+    for (int i = 0; i < num.Length/2; i++){
+        if (num[i] == num[num.Length - 1 - i] && flag != false){
+            flag = true;
+        }
+        else{
+            flag = false;
+        }
+    }
+    return (flag ? "Палиндром" : "Не палиндром");
+}
+
+Console.Write("Введите число: ");
+Console.Write(PalyndromeNum());
+
+
+
