bfdf5820c14263bfc507d52db914c5f40d0664ee
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-23 17:41:02 +0300

    Added Task23 solution

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int Prompt(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string CubeLine(int number){
+    string cubeLine = $"{number} -> ";
+    if (number > 0){
+        for(int i=1; i<=number; i++){
+        cubeLine += Convert.ToString(i*i*i) + " ";
+        }
+    }
+    else{
+        for(int i=-1; i>=number; i--){
+        cubeLine += Convert.ToString(i*i*i) + " ";
+        }
+    }
+    return cubeLine;
+}
+
+Console.WriteLine(CubeLine(Prompt("Введите число: ")));
